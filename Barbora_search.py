a4e16a08bdf9ebf3ed3cf83e10188ad15cfd18b8
wishalta <rolandas.lebed@gmail.com>
2024-11-27 14:18:58 +0200

    improved

--- Barbora_search.py
+++ Barbora_search.py
@@ -30,16 +30,38 @@ class barbora_search():
         return corrected_uppercase_words
 
     def search_quantity(self):
-        title1 = self.driver.find_element(By.CLASS_NAME, 'b-product-info--title').text
-        quantity = re.findall(r'\b\d+\b|\b[a-zA-Z]\b', title1)
-        quantity_answer = ' '.join(quantity)
-        return quantity_answer
+        # -----------------------------------VEIKIA-------------------------------------------
+        if True:
+            title1 = self.driver.find_element(By.CLASS_NAME, 'b-product-info--title').text
+            dl_element = self.driver.find_element(By.CLASS_NAME, "b-dl-align-left")
+            info_dict = {}
+            I_need = 'Grynasis kiekis'
+            dt_elements = dl_element.find_elements(By.TAG_NAME, "dt")
+            dd_elements = dl_element.find_elements(By.TAG_NAME, "dd")
+            for dt, dd in zip(dt_elements, dd_elements):
+                key = dt.text.strip()
+                value = dd.text.strip()
+                info_dict[key] = value
+            print(info_dict)
+            if info_dict.get('Grynasis kiekis (g/ml):') is not None:
+                return info_dict.get('Grynasis kiekis (g/ml):')
+            else:
+                # return 'Nebera'
+                quantity = re.findall(r'\b\d+\b|\b[a-zA-Z]\b', title1)
+                quantity_answer = ' '.join(quantity)
+                return quantity_answer
+
+        # title1 = self.driver.find_element(By.CLASS_NAME, 'b-product-info--title').text
+        # else:
+        #     quantity = re.findall(r'\b\d+\b|\b[a-zA-Z]\b', title1)
+            # quantity_answer = ' '.join(quantity)
+            # return quantity_answer
 
     def search_price(self):
         price = []
         price = self.driver.find_element(By.XPATH, '//*[@id="fti-product-price--0"]/div[1]/div[1]').text
         corrected_price = re.sub(r'\s+', '', price).strip()
-        print(corrected_price)
+        # print(corrected_price)
         return corrected_price
 
 
